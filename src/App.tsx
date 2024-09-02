da137e565d83e0c2a07f09d334e4cc3fd7c8e825
Davi Oliveira <davioliveirasilva.br@gmail.com>
2024-09-02 12:09:02 -0300

    Some margin added

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import WhoIAm from './components/WhoIAm'
 function App() {
   return <>
     <div className='flex flex-col items-center min-h-screen bg-gradient-to-bl from-white bg-slate-200 justify-center'>
-      <div className='flex flex-col lg:flex-row gap-16 grow justify-center items-center'>
+      <div className='flex flex-col lg:flex-row gap-16 mt-7 mb-7 grow justify-center items-center'>
         <Card></Card>
         <WhoIAm></WhoIAm>
       </div>      
